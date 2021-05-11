e5dea0e2802ea3b657421900b52ef0ff8666d384
Pedro Martins <pedrovrima@gmail.com>
2021-05-11 09:58:53 -0300

    few more bugs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const parseFields = require("graphql-parse-fields");
 const prismaFunctions = require("../src");
 
 const autoResolver = async (args, info, prisma) => {
-  const query_tree = prismaFunctions.parseFields(info, false);
+  const query_tree = parseFields(info, false);
   const prismaTree = prismaFunctions.getPrismaTree(query_tree, args);
   const prismaQuery = prismaFunctions.createQuery(prismaTree, prisma);
   const queryResult = await prismaQuery();
